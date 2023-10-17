1bc42c6024d30b582b4b810fbca31b7d56c5d886
Flund <f.lundsten@hotmail.se>
2023-10-17 19:03:49 +0200

    Fixed the layout and made stage not resizable

--- src/main/java/com/iths/tictactoe/HelloApplication.java
+++ src/main/java/com/iths/tictactoe/HelloApplication.java
@@ -12,7 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("TicTacToe!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
