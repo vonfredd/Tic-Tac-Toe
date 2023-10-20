bcc2313eaa56bd915bfecd956cc4fc163a19e006
Flund <f.lundsten@hotmail.se>
2023-10-20 15:40:14 +0200

    Remade game, mostly in model now

--- src/main/java/com/iths/tictactoe/HelloController.java
+++ src/main/java/com/iths/tictactoe/HelloController.java
@@ -6,9 +6,9 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.File;
@@ -62,13 +62,16 @@ public class HelloController {
         winnerName.textProperty().bind(model.winnerNameProperty());
         playerScore.textProperty().bind(model.playerScoreProperty().asString());
         computerScore.textProperty().bind(model.computerScoreProperty().asString());
+        buttons = new ArrayList<>();
         addButtons(buttons);
         setOpacityOnButtons(buttons);
+
         model.setGameIsOver(false);
-        model.setCount(9);
+        model.setEmptySpotsLeft(9);
+        model.setPlayerTurn(1);
     }
 
-    private void addButtons(List<Button> buttons){
+    private void addButtons(List<Button> buttons) {
         buttons.add(b1);
         buttons.add(b2);
         buttons.add(b3);
@@ -79,20 +82,82 @@ public class HelloController {
         buttons.add(b8);
         buttons.add(b9);
     }
-    private void setOpacityOnButtons(List<Button> buttons){
-        buttons.stream().forEach((e)-> e.setOpacity(1));
+
+    private void setOpacityOnButtons(List<Button> buttons) {
+        buttons.stream().forEach((e) -> e.setOpacity(1));
     }
 
     public void setXandO(MouseEvent event) {
-        model.XandO(event);
+        model.setPlayerTurn(1);
+        Button clickedButton = (Button) event.getSource();
+        clickedButton.setText(model.getPlayerOne());
+        clickedButton.setDisable(true);
+        model.setEmptySpotsLeft(model.getEmptySpotsLeft() - 1);
+
+        winner(model.getWinConditions(), buttons);
+
+        if (!model.isGameIsOver()) {
+            model.setPlayerTurn(0);
+            computerPick();
+            winner(model.getWinConditions(), buttons);
+        }
     }
 
-    public void resetGame(MouseEvent event) {
-        model.resetGame();
+    public void computerPick() {
+        if (model.getEmptySpotsLeft() == 0) {
+            model.setWinnerName("NO WINNER!");
+            model.setGameIsOver(true);
+            return;
+        }
+        int index = model.computerPickTwo(buttons.size() - 1);
+        while (buttons.get(index).isDisabled())
+            index = model.computerPickTwo(buttons.size() - 1);
+        buttons.get(index).setText(model.getPlayerTwo());
+        buttons.get(index).setDisable(true);
+        model.setEmptySpotsLeft(model.getEmptySpotsLeft() - 1);
     }
 
-    public void resetRound(MouseEvent event) {
-        model.resetRound();
+    public void resetGame() {
+        resetRound();
+        model.setPlayerScore(0);
+        model.setComputerScore(0);
+    }
+
+    public void resetRound() {
+        resetButtons(buttons);
+        model.setGameIsOver(false);
+        model.setWinnerName("");
+        model.setEmptySpotsLeft(9);
+    }
+
+    private static void resetButtons(List<Button> buttons) {
+        buttons.stream().forEach((e) -> {
+            e.setBorder(null);
+            e.setDisable(false);
+            e.setText("");
+        });
+    }
+
+    private void winner(int[][] winCondition, List<Button> buttons) {
+        StringBuilder possibleWinner;
+        List<Button> colorWinners;
+        for (int i = 0; i < winCondition.length; i++) {
+            colorWinners = new ArrayList<>();
+            possibleWinner = new StringBuilder();
+            for (int j = 0; j < winCondition[i].length; j++) {
+                possibleWinner.append(buttons.get(winCondition[i][j]).getText());
+                colorWinners.add(buttons.get(winCondition[i][j]));
+            }
+            if (possibleWinner.toString().equals("XXX") || possibleWinner.toString().equals("OOO")) {
+                paintWinningButtons(colorWinners);
+                model.theWinner(model.getPlayerTurn());
+            }
+        }
+    }
+
+    private void paintWinningButtons(List<Button> colorWinners) {
+        colorWinners.stream().forEach((e) -> e.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, new CornerRadii(5), new BorderWidths(5)))));
+        colorWinners.stream().forEach((e) -> e.setOpacity(1));
     }
 
     public void onSaveButtonAction(ActionEvent event) {
@@ -104,7 +169,7 @@ public class HelloController {
                 new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         File selectedFile = fileChooser.showOpenDialog(window);
         if (selectedFile != null) {
-          //  model.saveToFile(selectedFile);
+            //  model.saveToFile(selectedFile);
         }
     }
 }
--- src/main/java/com/iths/tictactoe/Model.java
+++ src/main/java/com/iths/tictactoe/Model.java
@@ -1,43 +1,17 @@
 package com.iths.tictactoe;
 
 import javafx.beans.property.*;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Model {
-    private int count;
+    public IntegerProperty playerTurn = new SimpleIntegerProperty();
+    private IntegerProperty emptySpotsLeft = new SimpleIntegerProperty();
     private StringProperty winnerName = new SimpleStringProperty();
-    private int[][] arr = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-    private StringProperty message = new SimpleStringProperty();
+    private final int[][] winConditions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
     private final ReadOnlyStringProperty playerOne = new ReadOnlyStringWrapper("X");
     private final ReadOnlyStringProperty playerTwo = new ReadOnlyStringWrapper("O");
     private BooleanProperty gameIsOver = new SimpleBooleanProperty();
-    private ObservableList<Button> buttons = FXCollections.observableList(new ArrayList<>());
-
     private IntegerProperty playerScore = new SimpleIntegerProperty();
     private IntegerProperty computerScore = new SimpleIntegerProperty();
-
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public void addButtons(Button button) {
-        buttons.add(button);
-        button.setOpacity(1);
-    }
-
 
     public String getPlayerOne() {
         return playerOne.get();
@@ -91,114 +65,41 @@ public class Model {
         this.gameIsOver.set(gameIsOver);
     }
 
-    public void XandO(MouseEvent event) {
-        Button clickedButton = (Button) event.getSource();
-        clickedButton.setText(getPlayerOne());
-        clickedButton.setDisable(true);
-        --count;
-        winner(arr, buttons);
-        if (!isGameIsOver()) {
-            computerPick();
-            winner(arr, buttons);
-        }
+    public int getEmptySpotsLeft() {
+        return emptySpotsLeft.get();
     }
 
-    public void computerPick() {
-        if (count == 0) {
-            setWinnerName("NO WINNER!");
+    public void setEmptySpotsLeft(int count) {
+        this.emptySpotsLeft.set(count);
+    }
+
+    public int[][] getWinConditions() {
+        return winConditions;
+    }
+
+    public int getPlayerTurn() {
+        return playerTurn.get();
+    }
+
+
+    public void setPlayerTurn(int playerTurn) {
+        this.playerTurn.set(playerTurn);
+    }
+    public Integer computerPickTwo(int numberOfGameSpots) {
+        return (int) (Math.random() * numberOfGameSpots);
+    }
+
+    public void theWinner(int winnersTurn) {
+        if (winnersTurn == 1) {
+            setWinnerName("Player Wins");
+            setPlayerScore(getPlayerScore() + 1);
             setGameIsOver(true);
-            return;
-        }
-        int choise = (int) (Math.random() * buttons.size() - 1);
-        calculateMove(choise);
-    }
-
-    private void calculateMove(int choise) {
-        while (buttons.get(choise).isDisabled()) {
-            choise = (int) (Math.random() * buttons.size());
-        }
-        buttons.get(choise).setText(getPlayerTwo());
-        buttons.get(choise).setDisable(true);
-        --count;
-    }
-
-
-    private void winner(int[][] winCondition, ObservableList<Button> buttons) {
-        StringBuilder possibleWinner;
-        List<Button> tempList;
-        for (int i = 0; i < winCondition.length; i++) {
-            tempList = new ArrayList<>();
-            possibleWinner = new StringBuilder();
-            for (int j = 0; j < winCondition[i].length; j++) {
-                possibleWinner.append(buttons.get(winCondition[i][j]).getText());
-                tempList.add(buttons.get(winCondition[i][j]));
-            }
-            if (possibleWinner.toString().equals("XXX")) {
-                paintWinningButtons(tempList);
-                setWinnerName("Player Wins");
-                setGameIsOver(true);
-                setPlayerScore(getPlayerScore() + 1);
-
-            } else if (possibleWinner.toString().equals("OOO")) {
-                paintWinningButtons(tempList);
-                setWinnerName("Computer Wins");
-                setGameIsOver(true);
-                setComputerScore(getComputerScore() + 1);
-            }
-        }
-    }
-
-    public void resetGame() {
-        resetRound();
-        setPlayerScore(0);
-        setComputerScore(0);
-    }
-
-    private static void resetButtons(List<Button> b) {
-        b.stream().forEach((e) -> {
-            e.setBorder(null);
-            e.setDisable(false);
-            e.setText("");
-        });
-    }
-
-    public void resetRound() {
-        resetButtons(buttons);
-        setGameIsOver(false);
-        setWinnerName("");
-        count = 9;
-    }
-
-    public void saveToFile(Path path) {
-
-        Thread thread = new Thread(() -> {
-            try {
-                Thread.sleep(20000);
-                Files.writeString(path, "HEHEHEHE");
-                System.out.println("File saved");
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        thread.start();
-    }
-
-    private static void paintWinningButtons(List<Button> tempList) {
-        tempList.stream().forEach((e) -> e.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, new CornerRadii(5), new BorderWidths(5)))));
-        tempList.stream().forEach((e) -> e.setOpacity(1));
-    }
-
-    public void difficultyHard() {
-        if (count == 0) {
-            setWinnerName("NO WINNER!");
+        } else if (winnersTurn == 0) {
+            setWinnerName("Computer Wins");
+            setComputerScore(getComputerScore() + 1);
             setGameIsOver(true);
-            return;
         }
-        int choise = (int) (Math.random() * buttons.size() - 1);
-        calculateMove(choise);
-
     }
-
 
 
 }
