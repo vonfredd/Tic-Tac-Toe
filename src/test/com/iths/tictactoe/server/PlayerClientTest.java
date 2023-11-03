9d8b18cc499fb3994aa40defe82e232f27b42d08
Flund <f.lundsten@hotmail.se>
2023-11-03 10:52:47 +0100

    remove test faulty method from client

--- src/test/com/iths/tictactoe/server/PlayerClientTest.java
+++ src/test/com/iths/tictactoe/server/PlayerClientTest.java
@@ -8,11 +8,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerClientTest {
 
-    @Test
-    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
-        PlayerClient client = new PlayerClient();
-        client.startConnection("127.0.0.1", 5555);
-        String response = client.sendMessage("hello server");
-        assertEquals("hello server", response);
-    }
 }
